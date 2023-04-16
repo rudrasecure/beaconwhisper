e93a50cec2f990b8c01e6ba981e928775f14617c
Hazzardousmonk <ks@rudrasecure.com>
2023-04-16 18:09:15 +0800

    Assign interface via sys argv

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ from components.beacon_frame_processor import BeaconFrameProcessor
 from components.pvb_maker import PVBMaker
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python main.py <transmit|receive> <filename>")
+    if len(sys.argv) != 4:
+        print("Usage: python main.py <transmit|receive> <filename>  <interface>")
         sys.exit(1)
 
     mode = sys.argv[1]
     filename = sys.argv[2]
+    interface = sys.argv[3]
 
     if mode not in ["transmit", "receive"]:
         print("Invalid mode. Use 'transmit' or 'receive'.")
         sys.exit(1)
 
     if mode == "transmit":
-        transmit(filename)
+        transmit(filename, interface)
     else:
-        receive(filename)
+        receive(filename, interface)
 
-def transmit(filename):
+def transmit(filename, interface):
     print(f"Transmitting file: {filename}")
     chunker = FileChunker(filename)
     chunks = chunker.read_and_chunk()
     pvb_array_maker = PVBMaker(chunks)
     pvb_array = pvb_array_maker.do()
-    interface = "wlan0mon"  # Change this to your wireless interface name
     sniffer = BeaconFrameProcessor(interface)
     sniffer.capture_beacon_frames(timeout=10)  # Capture unique beacon frames for 20 seconds
     sniffer.inject_tim_element(pvb_array)
@@ -37,9 +37,9 @@ def transmit(filename):
     sniffer.replay_beacon_frames()
 
 
-def receive(filename):
+def receive(filename, interface):
     print(f"Receiving file: {filename}")
-    receiver = BeaconFrameProcessor("wlan0mon")
+    receiver = BeaconFrameProcessor(interface)
     receiver.receive_exfilterated_data(timeout=10)
     receiver.write_file(filename)
 
